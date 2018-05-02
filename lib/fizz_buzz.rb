04cc0cdcb6383076cdf0113b86c7bca8b0582fee
Himalee <himalee.tailor@gmail.com>
2018-05-02 14:15:11 +0100

    One returns one

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,7 +1,10 @@
 class FizzBuzz
 
   def self.divisible_by(number)
-    0
+    if number == ""
+      0
+    else return number.to_i
+    end
   end
 
 
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -6,7 +6,13 @@ describe FizzBuzz do
       it "returns zero" do
         expect(FizzBuzz.divisible_by("")).to eql(0)
       end
-
     end
+
+    context "given one" do
+      it "returns one" do
+        expect(FizzBuzz.divisible_by("1")).to eql(1)
+      end
+    end
+
   end
 end

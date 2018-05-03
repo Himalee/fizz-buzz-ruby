0c2318d83f71c010296cc6e503cdf0734b1f47df
Himalee <himalee.tailor@gmail.com>
2018-05-03 11:49:06 +0100

    Reduced number of contexts

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,57 +1,45 @@
 require "fizz_buzz"
 
 describe FizzBuzz do
+
   describe ".divisible_by" do
-    context "given zero" do
-      it "returns zero" do
+
+    context "given number, returns number" do
+      it "zero returns zero" do
         expect(FizzBuzz.divisible_by(0)).to eql(0)
       end
-    end
-
-    context "given one" do
-      it "returns one" do
+      it "one returns one" do
         expect(FizzBuzz.divisible_by(1)).to eql(1)
       end
-    end
-
-    context "given two" do
-      it "returns two" do
+      it "two returns two" do
         expect(FizzBuzz.divisible_by(2)).to eql(2)
       end
     end
 
-    context "given three" do
-      it "returns Fizz" do
+    context "given multiple of three, returns Fizz" do
+      it "three returns Fizz" do
         expect(FizzBuzz.divisible_by(3)).to eql("Fizz")
       end
-    end
-
-    context "given six" do
-      it "returns Fizz" do
+      it "six returns Fizz" do
         expect(FizzBuzz.divisible_by(6)).to eql("Fizz")
       end
     end
 
-    context "given five" do
-      it "returns Buzz" do
+
+    context "given multiple of five, returns Buzz" do
+      it "five returns Buzz" do
         expect(FizzBuzz.divisible_by(5)).to eql("Buzz")
       end
-    end
-
-    context "given ten" do
-      it "returns Buzz" do
+      it "ten returns Buzz" do
         expect(FizzBuzz.divisible_by(10)).to eql("Buzz")
       end
     end
 
-    context "given fifteen" do
-      it "returns Fizz Buzz" do
+    context "given multiple of fifteen, returns Fizz Buzz" do
+      it "fifteen returns Fizz Buzz" do
         expect(FizzBuzz.divisible_by(15)).to eql("Fizz Buzz")
       end
-    end
-
-    context "given thirty" do
-      it "returns Fizz Buzz" do
+      it "thirty returns Fizz Buzz" do
         expect(FizzBuzz.divisible_by(30)).to eql("Fizz Buzz")
       end
     end

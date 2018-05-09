80caf956ba2c4d71ab1eef8e17d55ec7d391a135
Himalee <himalee.tailor@gmail.com>
2018-05-09 11:17:48 +0100

    Removing empty lines and updating contexts

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -11,5 +11,4 @@ class FizzBuzz
       number
     end
   end
-
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,10 +2,8 @@ require "fizz_buzz"
 
 describe FizzBuzz do
 
-  describe ".divisible_by" do
-
     context "given number, returns number" do
-      it "one returns one" do
+      it "returns one given one" do
         expect(FizzBuzz.divisible_by(1)).to eql(1)
       end
       it "two returns two" do
@@ -14,35 +12,32 @@ describe FizzBuzz do
     end
 
     context "given multiple of three, returns Fizz" do
-      it "three returns Fizz" do
+      it "returns Fizz given three" do
         expect(FizzBuzz.divisible_by(3)).to eql("Fizz")
       end
-      it "six returns Fizz" do
+      it "returns Fizz given six" do
         expect(FizzBuzz.divisible_by(6)).to eql("Fizz")
       end
     end
 
-
     context "given multiple of five, returns Buzz" do
-      it "five returns Buzz" do
+      it "returns Buzz given five" do
         expect(FizzBuzz.divisible_by(5)).to eql("Buzz")
       end
-      it "ten returns Buzz" do
+      it "returns Buzz given ten" do
         expect(FizzBuzz.divisible_by(10)).to eql("Buzz")
       end
     end
 
     context "given multiple of fifteen, returns Fizz Buzz" do
-      it "fifteen returns Fizz Buzz" do
+      it "returns Fizz Buzz given fifteen" do
         expect(FizzBuzz.divisible_by(15)).to eql("Fizz Buzz")
       end
-      it "thirty returns Fizz Buzz" do
+      it "returns Fizz Buzz given thirty" do
         expect(FizzBuzz.divisible_by(30)).to eql("Fizz Buzz")
       end
-      it "zero returns " do
+      it "returns Fizz Buzz given zero" do
         expect(FizzBuzz.divisible_by(0)).to eql("Fizz Buzz")
       end
     end
-
-  end
 end
